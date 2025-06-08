6b86d0b49a4b33bcc096155f9ba362c7048e6cf5
Salahi Erensel <salahi_erensel@hotmail.com>
2025-06-08 22:02:17 +0300

    Project Completed

--- OnlineSecureHospitalSystem/Migrations/20250601151943_LogsRemoved.Designer.cs
+++ OnlineSecureHospitalSystem/Migrations/20250601151943_LogsRemoved.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineSecureHospitalSystem.Data;
 
@@ -10,9 +11,11 @@ using OnlineSecureHospitalSystem.Data;
 namespace OnlineSecureHospitalSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601151943_LogsRemoved")]
+    partial class LogsRemoved
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.5");
